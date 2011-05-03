17c5739dbf17901014f3f1f186de503d77fb85ec
Todd Krause <toddbkrause@gmail.com>
2011-05-03 10:19:09 -0500

    Unpacking strings in NPA/NXX data

--- json/npa2gjson.rb
+++ json/npa2gjson.rb
@@ -12,7 +12,16 @@ require 'rgeo/geo_json'
 infile = File.new("npanxx.txt", "r")
 
 while (line = infile.gets) do
-  puts line
+  dataBits = line.match(/^(\d{3})\s*(\d{3})\s*(\d+\.\d+)\s*(\d+\.\d+)\s*(\D)\s*(\w+)\s*(\D+)$/)
+  aPlace = {
+    :npanxx => dataBits[1..2].join('-'),
+    :lat    => dataBits[3].to_f,
+    :long   => dataBits[4].to_f,
+    :wire   => dataBits[5],
+    :state  => dataBits[6],
+    :city   => dataBits[7]
+  }
+  puts aPlace[:city]
 end
 
 infile.close
