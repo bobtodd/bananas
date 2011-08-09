c0cad541514922a9bf0ddb8cc7fa0a64aaa8ebf3
Todd Krause <toddbkrause@gmail.com>
2011-08-09 12:00:24 -0500

    World Cup 2010 stats output as tsv

--- soccer/futbol.rb
+++ soccer/futbol.rb
@@ -10,11 +10,12 @@
 
 # an array of matches
 class Tournament
-  attr_accessor :name, :text
+  attr_accessor :name, :text, :phases
   
   def initialize(name, text)
-    @name = name
-    @text = clean(text)
+    @name   = name
+    @text   = clean(text)
+    @phases = find_phases
   end
 
   def clean(text)
@@ -26,8 +27,9 @@ class Tournament
     break_here = /[A-Z]*\s*PHASE|[A-Z]*\s*[A-Z]+\s*MATCH|[\dA-Z]*.*FINAL[S]*/
     breakpoints = @text.scan(break_here)
     
-    # create a hash for the phases:
-    # key="NAME OF PHASE", value=phase_text
+    # create an array for the phases, each entry as follows:
+    # ["NAME OF PHASE", phase_text], or more simply,
+    # each entry an instance of the Phase class
     phases = []
     for i in 0...breakpoints.length-1
       # take stuff after this breakpoint
@@ -45,11 +47,12 @@ class Tournament
 end
 
 class Phase
-  attr_accessor :name, :text
+  attr_accessor :name, :text, :groups
   
   def initialize(name, text)
-    @name = name
-    @text = clean(text)
+    @name   = name
+    @text   = clean(text)
+    @groups = find_groups
   end
 
   def clean(text)
@@ -66,7 +69,7 @@ class Phase
       breakpoints = @text.scan(break_here)
     
       for i in 0...breakpoints.length-1
-        puts "\tWorking on #{breakpoints[i]}" + "\t"*i + "..."
+        #puts "\tWorking on #{breakpoints[i]}" + "\t"*i + "..."
         garbage, useful_text = @text.split(breakpoints[i])
         useful_text, garbage = useful_text.split(breakpoints[i+1])
         
@@ -78,7 +81,7 @@ class Phase
         groups << Group.new(breakpoints[i], its_a_keeper.strip)
       end
       
-      puts "\tWorking on #{breakpoints.last}..."
+      #puts "\tWorking on #{breakpoints.last}..."
       tail = ""
       garbage.each_line do |line|
         tail << line unless line =~ /^\s\d\.[A-Z]+/
@@ -88,7 +91,7 @@ class Phase
     elsif @name != "FINAL"
       # Not in FIRST PHASE, but
       # not the FINAL either
-      puts "No longer FIRST PHASE......"
+      #puts "No longer FIRST PHASE......"
       no_blanks = ""
       @text.each_line do |line|
         no_blanks << line unless line =~ /^\s*$/
@@ -108,15 +111,87 @@ class Phase
 end
 
 class Group
-  attr_accessor :name, :text
+  attr_accessor :name, :text, :games
   
   def initialize(name, text)
-    @name = name
-    @text = text
+    @name  = name
+    @text  = text
+    @games = find_games
+  end
+  
+  def find_games()
+    # more or less, each line is a game
+    games = []
+    
+    if @name =~ /Group\s[A-Z]/
+      @text.each_line do |line|
+        date, text = line.scan(/(\s*\d+[-]\s*\d+[-]\d+)\s+(.+)$/)[0]
+        games << Game.new("#{@name}", date, text.strip)
+      end
+    elsif @name =~ /FINAL(?!S)/
+      location = ""
+      date = ""
+      text = ""
+      @text.each_line do |line|
+        if line =~ /^\w+[,]\s\w+\s\d+[,]\s\d+/
+          location, date = line.scan(/^(\w+)[,]\s(\w+\s\d+[,]\s\d+)/)[0]
+        else
+          text << location + "   " + line.strip
+        end
+      end
+      games << Game.new("#{@name}", date, text)
+    else
+      @text.each_line do |line|
+        if line =~ /^[\s\d]\d/
+          date, text = line.scan(/(\s*\d+[-]\s*\d+[-]\d+)\s+(.+)$/)[0]
+          games << Game.new("#{@name}", date, text.strip)
+        end
+      end
+    end
+    
+    return games
   end
 end
 
-class Match
+class Game
+  attr_accessor :name, :date, :text, :location, :home, :home_pts, :away, :away_pts, :notes
+  
+  def initialize(name, date, text)
+    @name = name
+    @date = date
+    @text = text.strip
+    @location, @home, @home_pts, @away, @away_pts, @notes = sift
+  end
+  
+  def sift
+    data = []
+    location           = @text[0..13].strip
+    home               = @text[15..26].strip
+    home_pts, away_pts = @text.scan(/(\d+)[-](\d+)/)[0]
+    away               = @text[37..49]
+    notes              = @text[50..-1] ? @text[50..-1].strip : ""
+    data << location
+    data << home
+    data << home_pts
+    data << away
+    data << away_pts
+    data << notes
+  end
+  
+  def score
+    puts @home + " " + @home_pts + "-" + @away_pts + " " + @away
+  end
+  
+  def to_s
+    outstr  = @name     + "\t"
+    outstr += @date     + "\t"
+    outstr += @location + "\t"
+    outstr += @home     + "\t"
+    outstr += @home_pts + "\t"
+    outstr += @away     + "\t"
+    outstr += @away_pts + "\t"
+    outstr += @notes
+  end
 end
 
 require 'nokogiri'
@@ -135,13 +210,12 @@ puts "outfilename: #{outfilename}"
 
 wbpg.xpath("//pre").each do |text|
   tourney = Tournament.new(title, text)
-  # puts tourney.text
-  phases = tourney.find_phases
-  for ph in phases
-    puts "Phase #{ph.name}: finding groups..."
-    groups = ph.find_groups
-    groups.each do |grp|
-      puts "Phase #{ph.name}: #{grp.name}:\n#{grp.text}"
+  tourney.phases.each do |ph|
+    puts "Phase: #{ph.name}"
+    ph.groups.each do |grp|
+      grp.games.each do |gm|
+        puts gm.to_s
+      end
     end
   end
 end
